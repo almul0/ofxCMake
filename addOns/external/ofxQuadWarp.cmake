89715f1008150f96fdea7651524a8aee0f4ecb1d
Alberto Mur <565825@unizar.es>
2017-11-17 14:28:45 +0100

    Addon configuration

--- addOns/external/ofxQuadWarp.cmake
+++ addOns/external/ofxQuadWarp.cmake
@@ -1,39 +1,40 @@
-# -----------------------------------------------------------------
-# --- Set the name of your AddOn
-# -----------------------------------------------------------------
+# =================================================================
+#
+# Template for addOns
+#
+# Easiest way: replace all the NAME into the name of your addOn
+# It has to have the same name, as the directory it is in
+# [e.g. ofxOneEuroFilter is in of/addons/ofxOneEuroFilter/]
+#
+# =================================================================
 
-set( NAME_ADDON     ofxQuadWarp )
+set( NAME_ADDON     ofxQuadWarp )       # <--- Set the name here
 
 #==================================================================
-#==================================================================
+
+
 # -----------------------------------------------------------------
 # ---------------------------- PATHS ------------------------------
 # -----------------------------------------------------------------
-set( PATH_SOURCE    ${OF_DIRECTORY_ABSOLUTE}/addons/${NAME_ADDON}/src )
+set( PATH_SRC    ${OF_DIRECTORY_ABSOLUTE}/addons/${NAME_ADDON}/src )
 set( PATH_LIBS      ${OF_DIRECTORY_ABSOLUTE}/addons/${NAME_ADDON}/libs )
-
-# --- Setting abolute path to prevent errors
-get_filename_component( PATH_SOURCE_ABSOLUTE ${PATH_SOURCE} ABSOLUTE)
-get_filename_component( PATH_LIBS_ABSOLUTE ${PATH_LIBS} ABSOLUTE)
 
 # -----------------------------------------------------------------
 # ---------------------------- SOURCE -----------------------------
 # -----------------------------------------------------------------
 
-file( GLOB_RECURSE   OFX_ADDON_CPP          "${PATH_SOURCE_ABSOLUTE}/*.cpp" )
-file( GLOB_RECURSE   OFX_ADDON_LIBS_CPP     "${PATH_LIBS_ABSOLUTE}/*.cpp" )
-add_library(  ${NAME_ADDON}   STATIC   ${OFX_ADDON_CPP} ${OFX_ADDON_LIBS_CPP} )
+
+file( GLOB_RECURSE   OFX_ADDON_CPP          "${PATH_SRC}/*.cpp" )
+file( GLOB_RECURSE   OFX_ADDON_LIBS_CPP     "${PATH_LIBS}/*.cpp" )
+add_library(  ${NAME_ADDON}   STATIC  ${OFX_ADDON_CPP} ${OFX_ADDON_LIBS_CPP} )
+set(OpenCV_DIR "${OF_DIRECTORY_ABSOLUTE}/addons/ofxOpenCv/libs/opencv")
+find_package(OpenCV REQUIRED COMPONENTS core imgproc highgui calib3d)
+target_link_libraries(${NAME_ADDON} ${OpenCV_LIBS})
 
 # -----------------------------------------------------------------
-# ---------------------------- HEADERS ----------------------------
-# -----------------------------------------------------------------
 
-OF_find_header_directories( HEADERS_SOURCE ${PATH_SOURCE_ABSOLUTE} )
+OF_find_header_directories( HEADERS_SOURCE ${PATH_SRC} )
+OF_find_header_directories( HEADERS_LIBS ${PATH_LIBS} )
+
 include_directories( ${HEADERS_SOURCE} )
-
-OF_find_header_directories( HEADERS_LIBS ${PATH_LIBS_ABSOLUTE} )
 include_directories( ${HEADERS_LIBS} )
-
-# -----------------------------------------------------------------
-# ------------------------------ LIBS  ----------------------------
-# -----------------------------------------------------------------
